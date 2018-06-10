99e7744e54e4cf6513792ff6eabb5b93ae3d5a3b
Andrew Arbogast <ndrwrbgs@users.noreply.github.com>
2018-06-09 19:35:25 -0700

    Add nuget generation

---
+++ nuget/GeneratePackage.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo This script must be run from 'Developer Command Prompt for VS 2017' or similar developer environment.
+msbuild %~dp0\..\src\Library\Library.csproj /p:Configuration=Release /v:m
+
+REM exit if if failed
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy /Y %~dp0\..\src\Library\bin\Release\net4.5.1\FastLinq.dll %~dp0\lib\net45
+xcopy /Y %~dp0\..\src\Library\bin\Release\net4.5.1\FastLinq.pdb %~dp0\lib\net45
+
+echo.
+echo.
+nuget pack %~dp0\Package.nuspec -OutputDirectory %~dp0
